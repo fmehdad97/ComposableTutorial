5e682c9a50a2072114611c006a5e4fbe1b1d91ad
Fayez Mehdad <fayez.mehdad@gmail.com>
2024-01-27 13:49:41 +0600

    Experiment with layout and images in compose

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,26 +3,44 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Android")
+            MessageCard(Message("Android","What is it good for"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String){
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message){
+    Row{
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+        )
+
+        Column{
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard(){
-    MessageCard(name = "who is")
+    MessageCard(
+        msg = Message("Lexi", "Who that?")
+    )
 }
